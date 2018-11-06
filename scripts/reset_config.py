aa2fcbe8b65b5287f353972a7b2e7dff00749c50
abpwrs <alexander-powers@uiowa.edu>
2018-11-05 20:42:38 -0600

    fixed config

--- scripts/reset_config.py
+++ scripts/reset_config.py
@@ -5,7 +5,8 @@ config = {
     "data_dir":str(os.path.join(pwd,"data")),
     "model_dir":str(os.path.join(pwd,"model")),
     "out_dir":str(os.path.join(pwd,"out")),
-    "job_dir":str(os.path.join(pwd,"out"))
+    "job_dir":str(os.path.join(pwd,"job"))
 }
+
 with open(os.path.join(pwd,"config.json"), "w") as f:
     json.dump(config,f, indent=4)
